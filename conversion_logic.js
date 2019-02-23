23f6833eecfb96ff79c5979b50648751e3d78fab
Anastasia Bourlas (Stasia) <bourlas.3@osu.edu>
2019-02-22 23:37:10 -0500

    Update conversion_logic.js

--- conversion_logic.js
+++ conversion_logic.js
@@ -3,14 +3,15 @@ function glyphEnter() {
   var glyph = document.getElementById("glyph").value;
   var binary = document.getElementById("be").value;
   var codepoint = document.getElementById("cp").value;
-  var char = document.getElementById("char").value;
 }
 function cpEnter() {
   var codepoint = document.getElementById("cp").value;
   document.getElementById("glyph").value=String.fromCodePoint(codepoint);
-  document.getElementById("be").value=parseInt(codepoint,2);
+  document.getElementById("be").value=codepoint.toString(2);
 }
 
 function beEnter() {
   var binary = document.getElementById("be").value;
+  document.getElementById("cp").value=binary.toString(10);
+  document.getElementById("glyph").value=String.fromCodePoint(binary.toString(10));
 }
